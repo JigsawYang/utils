0e7b4d7103230e5fdde9d2ea145ef6ef5af78470
Chris <jigsawchris016@gmail.com>
2015-01-08 23:34:56 +0800

    rand str to mysql

---
+++ rand_mysql.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+#coding:utf-8
+import pymysql
+import random, time
+import zlib
+
+def randstr_to_mysql(times = 10):
+    ranstr = (''.join(random.sample('zyxwvutsrqponmlkjihgfedcba', 20)));
+    rancrc = zlib.crc32(ranstr.encode())
+
+    conn = pymysql.connect('localhost', 'root', '2103189', 'h_perfor')
+    cur = conn.cursor()
+    num = 0
+    while num < times:
+        ran = random.randint(1, 100)
+        ranstr = ''.join(random.sample('zyxwvutsrqponmlkjihgfedcba', 20))
+        rancrc = zlib.crc32(ranstr.encode())
+        cur.execute("insert into test(num, randstr, crcstr) values (%s, %s, %s)", (str(ran), ranstr, rancrc))
+        num = num + 1
+    conn.commit()
+    cur.close()
+    conn.close()
+
+if __name__ == '__main__':
+    randstr_to_mysql();
+
